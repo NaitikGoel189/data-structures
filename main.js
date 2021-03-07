4b4dd88a9b9d075408c28d261d05924146f0d6e9
NaitikGoel189 <74708219+NaitikGoel189@users.noreply.github.com>
2021-03-07 14:06:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var name_of_family_members_array = [];
+
+function submit(){
+    var name1 = document.getElementById("name_1").value;
+    var name2 = document.getElementById("name_2").value;
+    var name3 = document.getElementById("name_3").value;
+    var name4 = document.getElementById("name_4").value;
+
+name_of_family_members_array.push(name1);
+name_of_family_members_array.push(name2);
+name_of_family_members_array.push(name3);
+name_of_family_members_array.push(name4);
+
+console.log(name_of_family_members_array);
+document.getElementById("display_name").innerHTML=name_of_family_members_array;
+document.getElementById("button_submit").style.display="none";
+document.getElementById("button_sort").style.display="inline-block";
+}
+
+function sorting(){
+    name_of_family_members_array.sort();
+    console.log(name_of_family_members_array);
+    document.getElementById("display_name").innerHTML=name_of_family_members_array; 
+}
